5655308ec7d6e89ebcfc651d44e68fbc02f37b04
Minbu5 <arkk250@gmail.com>
2025-02-18 08:41:42 +0200

    Add coupe layout.png and locate seat position main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # When travel with tran 3rd class there is always question where is my seat? Is at window or not?
 # This off course can be adjusted during buy process, but let's say you forgot.
 # Make small app that could be run on multiple platforms mobile android
-# that can tell you where is your seat is at window, in the middle or at passage
-# For layout of seats in train see picture in train layout.png
+# that can tell you where is your seat: is at window, in the middle or at passage
+# For layout of seats in train see picture in train layout.png and coupe numbering logic in coupe layout.png
 
 
 def seat_position(seat):
